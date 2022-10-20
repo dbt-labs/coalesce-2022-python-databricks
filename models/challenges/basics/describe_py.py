78d821252548d3f860103aede37d83527c87fc71
Rijnhardt Kotze <rijnhardt@horizondata.co>
2022-10-20 15:40:54 +0000

    new: describe python model

---
+++ models/challenges/basics/describe_py.py
@@ -0,0 +1,12 @@
+def model(dbt, session):
+
+    # get upstream data
+    orders = dbt.ref("orders").pandas_api()
+
+    # describe the data
+    described = orders.describe()
+
+    # insert the index as the first column
+    described.insert(0, "metric", described.index)
+
+    return described
