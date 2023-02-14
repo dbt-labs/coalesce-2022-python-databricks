d00815d04df8664c699a66e9733203055809f9da
Sean McIntyre <sean.mcintyre@fishtownanalytics.com>
2023-02-14 14:30:34 +0000

    Got the first challenge to work!

--- models/challenges/basics/pivot_py.py
+++ models/challenges/basics/pivot_py.py
@@ -1,19 +1,20 @@
 def model(dbt, session):
-
-    # comment this out to enable the model
-    dbt.config(enabled=False)
-
     # get upstream data
-    orders = dbt.ref("orders").pandas_api()
-    products = dbt.ref("stg_products").pandas_api()
-    order_items = dbt.ref("stg_order_items").pandas_api()
+    orders = dbt.ref("orders").toPandas()
+    products = dbt.ref("stg_products").toPandas()
+    order_items = dbt.ref("stg_order_items").toPandas()
 
     # list of unique product ids
-    product_ids = None  # TODO: fix this
+    product_ids = sorted(list(set(products["product_id"].unique())))
 
     # get the subtotal for each product_id for each order_id
     order_item_product_subtotals = (
-        # TODO: fix this
+        order_items.merge(products, on="product_id")
+        .groupby(["order_id", "product_id"])
+        .agg(subtotal=("product_price", "sum"))
+        .reset_index()
+        .pivot(index="order_id", columns="product_id", values="subtotal")
+        .reset_index()
     )
 
     # rename the product_id columns to include "subtotal_"
